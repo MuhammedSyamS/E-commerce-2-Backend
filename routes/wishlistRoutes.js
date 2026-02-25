cf8054caf2ff68e40e8450437d64ce216cb7c8b2
Muhammed Syam <shamsaifudheen@gmail.com>
2026-02-25 15:44:53 +0530

    some changes in directory

--- routes/wishlistRoutes.js
+++ routes/wishlistRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { toggleWishlist, getWishlist } = require('../controllers/WishlistController');
+const { toggleWishlist, getWishlist } = require('../controllers/wishlistController');
 const { protect } = require('../middleware/authMiddleware');
 
 // This handles POST /api/wishlist
